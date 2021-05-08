2190690472fe0b70288c7d8862158d285094a5c1
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-05-08 15:21:56 -0400

    Add Security Configuration

---
+++ src/main/java/com/meritamerica/assignment7/Assignment7Application.java
@@ -0,0 +1,15 @@
+package com.meritamerica.assignment7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories("com.meritamerica.assignment7.repository")
+@SpringBootApplication
+public class Assignment7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment7Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/meritamerica/assignment7/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.meritamerica.assignment7.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping("/")
+	public String Home(){
+		return "Welcome";
+	}
+	
+	@PostMapping("/authenticate")
+	public String authenticate(){
+		return "authenticate";
+	}
+
+}
